dbb71ea29a61a0b5f3e4abc891e67a5375ff1969
bch <bollombollom@yahoo.fr>
2025-02-11 09:12:17 +0100

    fin du Romain

--- src/td/Romain.java
+++ src/td/Romain.java
@@ -10,4 +10,21 @@ public class Romain {
 		return nom;
 	}
 
+	public String prendreParole() {
+		return "Le romain " + getNom() + ":";
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + texte); // DRY
+	}
+
+	public void recevoirCoup(int forceCoup) {
+		force -= forceCoup; // effet 1
+		if (force > 0) {
+			parler("Aïe"); // effet 2
+		} else {
+			parler("J'abandonne");
+		}
+	}
+
 }
